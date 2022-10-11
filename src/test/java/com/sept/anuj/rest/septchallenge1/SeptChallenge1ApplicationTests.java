6132b32bf8d0c869ebb074e20f8c622f06836379
Anuj Sharma <s3820112@student.rmit.edu.au>
2022-10-12 05:48:29 +1100

    First Push

---
+++ src/test/java/com/sept/anuj/rest/septchallenge1/SeptChallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sept.anuj.rest.septchallenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptChallenge1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
